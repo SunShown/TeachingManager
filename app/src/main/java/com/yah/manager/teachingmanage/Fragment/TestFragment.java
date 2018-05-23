1f230440b787f4e327e8d421ea18984e74b0a8fe
liufeixuan <lfx7320@gmail.com>
2018-05-23 18:46:00 +0800

    [更新界面]

--- app/src/main/java/com/yah/manager/teachingmanage/Fragment/TestFragment.java
+++ app/src/main/java/com/yah/manager/teachingmanage/Fragment/TestFragment.java
@@ -6,18 +6,50 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterViewFlipper;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.yah.manager.teachingmanage.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2018/4/5.
  */
 
 public class TestFragment extends Fragment {
+    @BindView(R.id.img_pre)
+    ImageView imgPre;
+    @BindView(R.id.mytime)
+    Chronometer mytime;
+    @BindView(R.id.img_next)
+    ImageView imgNext;
+    @BindView(R.id.pb)
+    ProgressBar pb;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_num)
+    TextView tvNum;
+    @BindView(R.id.vf)
+    AdapterViewFlipper vf;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_test,null);
+        View view = inflater.inflate(R.layout.fragment_test, null);
+        unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
